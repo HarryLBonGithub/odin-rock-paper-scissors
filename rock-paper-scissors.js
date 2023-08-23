968b1d30e4ed0f4e0cc2bef89d9006029e23fe7f
Harry L-B <harryloefflerbell@gmail.com>
2023-08-22 22:45:14 -0500

    add computerChoice function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,14 @@
 function getComputerChoice(){
+    let options = ["rock","paper", "scissors"]
 
+    let roll = Math.floor(Math.random() * options.length)
+
+    return options[roll]
 }
+
+console.log(getComputerChoice())
+console.log(getComputerChoice())
+console.log(getComputerChoice())
+console.log(getComputerChoice())
+console.log(getComputerChoice())
+console.log(getComputerChoice())
